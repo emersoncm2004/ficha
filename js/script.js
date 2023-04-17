3ce3bc9ef7af7e3170d200874c4d4ffb2db72fb1
emersoncm2004 <cabreraemerson83@gmail.com>
2023-04-17 16:03:26 -0500

    ficha

---
+++ js/script.js
@@ -0,0 +1,134 @@
+
+
+function comprobar(){
+    let nombre, produccion, anterior, actual, usuarios, reportes, general, objetivos, descripcion, hardware, software, otros, hardware2, software2, otros2, adicional, funcional, clientes, uno, dos, tres, cuatro, cinco, seis, siete, ocho, aaa, bbb, ccc, ddd, eee, fff
+
+    nombre = document.getElementById("nombre").value
+    document.getElementById("respuesta1").innerHTML="<h3> Nombre del producto: "+nombre+"</h3>"
+    console.log(nombre)
+
+    produccion = document.getElementById("produccion").value
+    document.getElementById("respuesta2").innerHTML="<h3> Línea de producción: "+produccion+"</h3>"
+    console.log(produccion) 
+
+    anterior = document.getElementById("anterior").value
+    document.getElementById("respuesta3").innerHTML="<h3> Versiones anteriores: "+anterior+"</h3>"
+    console.log(anterior)
+
+    actual = document.getElementById("actual").value
+    document.getElementById("respuesta4").innerHTML="<h3> Versión actual: "+actual+"</h3>"
+    console.log(actual)
+
+    usuarios = document.getElementById("usuarios").value
+    document.getElementById("respuesta5").innerHTML="<h3> Usuarios: "+usuarios+"</h3>"
+    console.log(usuarios)
+
+    reportes = document.getElementById("reportes").value
+    document.getElementById("respuesta6").innerHTML="<h3> Reportes: "+reportes+"</h3>"
+    console.log(reportes)
+
+    general = document.getElementById("general").value
+    document.getElementById("respuesta7").innerHTML="<h3> Descripción general: "+general+"</h3>"
+    console.log(general)
+
+    objetivos = document.getElementById("objetivos").value
+    document.getElementById("respuesta8").innerHTML="<h3> Objetivos: "+objetivos+"</h3>"
+    console.log(objetivos)
+
+    descripcion = document.getElementById("descripcion").value
+    document.getElementById("respuesta9").innerHTML="<h3> Descripción: "+descripcion+"</h3>"
+    console.log(descripcion)
+
+    hardware = document.getElementById("hardware").value
+    document.getElementById("respuesta10").innerHTML="<h3> Hardware: "+hardware+"</h3>"
+    console.log(hardware)
+
+    software = document.getElementById("software").value
+    document.getElementById("respuesta11").innerHTML="<h3> Software: "+software+"</h3>"
+    console.log(software)
+
+    otros = document.getElementById("otros").value
+    document.getElementById("respuesta12").innerHTML="<h3> Otros: "+otros+"</h3>"
+    console.log(otros)
+
+    hardware2 = document.getElementById("hardware2").value
+    document.getElementById("respuesta13").innerHTML="<h3> Hardware: "+hardware2+"</h3>"
+    console.log(hardware2)
+
+    software2 = document.getElementById("software2").value
+    document.getElementById("respuesta14").innerHTML="<h3> Software: "+software2+"</h3>"
+    console.log(software2)
+
+    otros2 = document.getElementById("otros2").value
+    document.getElementById("respuesta15").innerHTML="<h3> Otros: "+otros2+"</h3>"
+    console.log(otros2)
+
+    adicional = document.getElementById("adicional").value
+    document.getElementById("respuesta16").innerHTML="<h3> Requerimiento adicional: "+adicional+"</h3>"
+    console.log(adicional)
+
+    funcional = document.getElementById("funcional").value
+    document.getElementById("respuesta17").innerHTML="<h3> Requerimiento funcional: "+funcional+"</h3>"
+    console.log(funcional)
+
+    clientes = document.getElementById("clientes").value
+    document.getElementById("respuesta18").innerHTML="<h3> Clientes del producto: "+clientes+"</h3>"
+    console.log(clientes)
+
+    uno = document.getElementById("uno").value
+    document.getElementById("respuesta19").innerHTML="<h3> Versión: "+uno+"</h3>"
+    console.log(uno)
+
+    dos = document.getElementById("dos").value
+    document.getElementById("respuesta20").innerHTML="<h3> Naturaleza: "+dos+"</h3>"
+    console.log(dos)
+
+    tres = document.getElementById("tres").value
+    document.getElementById("respuesta21").innerHTML="<h3> Fecha aprobación: "+tres+"</h3>"
+    console.log(tres)
+
+    cuatro = document.getElementById("cuatro").value
+    document.getElementById("respuesta22").innerHTML="<h3> Fecha validación: "+cuatro+"</h3>"
+    console.log(cuatro)
+
+    cinco = document.getElementById("cinco").value
+    document.getElementById("respuesta23").innerHTML="<h3> Versión: "+cinco+"</h3>"
+    console.log(cinco)
+
+    seis = document.getElementById("seis").value
+    document.getElementById("respuesta24").innerHTML="<h3> Naturaleza: "+seis+"</h3>"
+    console.log(seis)
+
+    siete = document.getElementById("siete").value
+    document.getElementById("respuesta25").innerHTML="<h3> Fecha aprobación: "+siete+"</h3>"
+    console.log(siete)
+
+    ocho = document.getElementById("ocho").value
+    document.getElementById("respuesta26").innerHTML="<h3> Fecha validación: "+ocho+"</h3>"
+    console.log(ocho)
+
+    aaa = document.getElementById("aaa").value
+    document.getElementById("respuesta27").innerHTML="<h3> Elaboró: "+aaa+"</h3>"
+    console.log(aaa)
+
+    bbb = document.getElementById("bbb").value
+    document.getElementById("respuesta28").innerHTML="<h3> Aprobó: "+bbb+"</h3>"
+    console.log(bbb)
+
+    ccc = document.getElementById("ccc").value
+    document.getElementById("respuesta29").innerHTML="<h3> Validó: "+ccc+"</h3>"
+    console.log(ccc)
+
+    ddd = document.getElementById("ddd").value
+    document.getElementById("respuesta30").innerHTML="<h3> Elaboró: "+ddd+"</h3>"
+    console.log(ddd)
+
+    eee = document.getElementById("eee").value
+    document.getElementById("respuesta31").innerHTML="<h3> Aprobó: "+eee+"</h3>"
+    console.log(eee)
+
+    fff = document.getElementById("fff").value
+    document.getElementById("respuesta32").innerHTML="<h3> Validó: "+fff+"</h3>"
+    console.log(fff)
+
+}
